4ad5762745859a2cec721b0521ead54709788b0a
Pushpraj Singh <psingh@imenso.tech>
2023-07-15 12:32:36 +0530

    complete

--- src/react-quilljs.js
+++ src/react-quilljs.js
@@ -4,12 +4,31 @@ import "quill/dist/quill.snow.css";
 
 export default function Editorjs() {
   const { quill, quillRef } = useQuill();
-  return (
+  const [data,setData]=React.useState();
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.on('text-change', (delta, oldDelta, source) => {
+        console.log('Text change!');
+        console.log(quill.getText()); // Get text only
+        console.log(quill.getContents()); // Get delta contents
+        console.log(quill.root.innerHTML); // Get innerHTML using quill
+        console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
+        setData(quillRef.current.firstChild.innerHTML);
+      });
+    }
+  }, [quill]);
+
+  return (<>
     <div style={{display:"flex",justifyContent:"center"}}>
         <div style={{width:"800px"}}>
           <h1 style={{textAlign:"center"}}>React Quill Js TextBox</h1>
       <div ref={quillRef} />
     </div>
+    <div>
     </div>
-  );
+    
+    </div>
+    <div className="ql-editor" dangerouslySetInnerHTML={{ __html:data }}></div>
+  </>);
 }
